bd8e2e67eaecef5c8cb8d1301c17a46d212e1a77
sean <eli01linux@aliyun.com>
2017-01-18 03:42:54 +0800

    windows test fine

--- JK_C-PLUS-PLUS/l03_class_encapsulation_two/0601/maze.cpp
+++ JK_C-PLUS-PLUS/l03_class_encapsulation_two/0601/maze.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "MazeMap.h"
+#include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
 int main(void)
 {
+	cout << "haha" << endl;
+	system("pause");
 	return 0;
 }
