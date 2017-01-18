91f3b699942f6e5219a398a298c9064979a9d16b
earth eli <earth eli>
2017-01-18 18:16:20 +0800

    MazePerson constructor

--- JK_C-PLUS-PLUS/l03_class_encapsulation_two/0601/MazePerson.h
+++ JK_C-PLUS-PLUS/l03_class_encapsulation_two/0601/MazePerson.h
@@ -3,4 +3,24 @@
 */
 
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include "Public.h"
+
 using namespace std;
+
+#ifndef MazePerson_H_
+#define MazePerson_H_
+
+const bool YES = true;
+const bool NO = false;
+
+class MazePerson
+{
+public:
+    MazePerson(); // 默认构造器
+    MazePerson(int currentX, int currentY); // 
+    MazePerson(int currentX, int currentY, objectiveDirections myForward);
+    MazePerson &setPersonPosition(int char personChar);    
+};
+#endif // MazePerson_H_
